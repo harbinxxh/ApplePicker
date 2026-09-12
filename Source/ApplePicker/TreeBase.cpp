99dca249ab7661620bac140e4d19b1b45dcab2eb
xuxh <harbinxxh@gmail.com>
2026-09-12 23:45:53 +0800

    30-04 苹果树基类定义

---
+++ Source/ApplePicker/TreeBase.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TreeBase.h"
+
+// Sets default values
+ATreeBase::ATreeBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TreeMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TreeMeshComponent"));
+
+	RootComponent = TreeMeshComponent;
+
+}
+
+// Called when the game starts or when spawned
+void ATreeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATreeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
